3c3fcb1783454d76e88d4040744ad88b7e33f95b
Aziz Kosber <a.kosber@gmail.com>
2018-05-26 17:55:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,31 +1,29 @@
 require 'pry'
 
-katz_deli = []
+deli = []
 
-def line(katz_deli)
-  if katz_deli.length == 0
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
   else
     line_status = "The line is currently:"
-    katz_deli.each_with_index do |person, index|
-      position = index + 1
-      line_status << " #{position}. #{person}"
+    deli.each.with_index(1) do |person, index|
+      line_status << " #{index}. #{person}"
     end
     puts line_status
   end
 end
 
-def take_a_number(katz_deli, name)
+def take_a_number(deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(katz_deli)
-  if katz_deli.length == 0
+def now_serving(deli)
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
   end
 end
-binding.pry
